a0bbb59129e5306741618b925533dacdbfcba234
learner-of-string <aref0171personal@gmail.com>
2024-10-31 15:55:57 +0600

    nav bar done with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: "#ABEF5F",
+          accent: "#ABEF5F80"
+        },
+      },
+    ],
+  },
   content: ["./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'),],
-}
+  plugins: [require("daisyui")],
+};
